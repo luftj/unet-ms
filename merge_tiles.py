3c156ad00d138893c3d052b1b0df4bb5cff6da95
Jonas <jonas.luft@hcu-hamburg.de>
2021-07-23 15:47:20 +0200

    allow merge of offset tiles

--- merge_tiles.py
+++ merge_tiles.py
@@ -23,6 +23,26 @@ def merge_image(name, dims, output_dir):
             tile_idx += 1
     full_img = full_img.crop([0,0,tile_size[0]*x+last_size[0],tile_size[1]*y+last_size[1]])
     full_img.save(output_dir+"/"+name+".png", "PNG")
+    
+def merge_image_xy(name, positions, output_dir):
+    tile_idx = 0
+    full_img=None
+    for position in positions:
+        x,y = position
+        print("merging %d,%d" % (x,y))
+        path ="%s/%s_tile%d-%d.tif" % (in_dir, name, x, y)
+        print(path)
+        cur_img = Image.open(path)
+        if not full_img:
+            tile_size = cur_img.size
+            # set canvas size
+            full_img=Image.new("RGB", (max(list(zip(*positions))[0]) + tile_size[0], max(list(zip(*positions))[1]) + tile_size[1]), (0,0,0))
+        full_img.paste(cur_img, (x,y))
+        tile_idx += 1
+    first_offset = (min(list(zip(*positions))[0]), min(list(zip(*positions))[1]))
+    print("cropping to",[*first_offset, *full_img.size])
+    full_img = full_img.crop([*first_offset, *full_img.size])
+    full_img.save(output_dir+"/"+name+".png", "PNG")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='',
@@ -45,5 +65,9 @@ if __name__ == "__main__":
         images_dict[name].append(tile_no)
     
     for name, tiles in images_dict.items():
-        merge_image(name, dims=(13,11), output_dir=args.outdir)
-        exit()
+        if "-" in tiles[0]:
+            positions = [tuple(map(int,x.split("-"))) for x in tiles]
+            print(positions)
+            merge_image_xy(name, positions, output_dir=args.outdir)
+        else:
+            merge_image(name, dims=(13,11), output_dir=args.outdir)
